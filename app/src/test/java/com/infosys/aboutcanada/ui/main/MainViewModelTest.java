3f465126c993a46239d491fbb7a2229f1e04315e
ABHIJIT BAGUL <abhibagul@bitbucket.org>
2021-11-21 21:18:24 +0530

    developement done

---
+++ app/src/test/java/com/infosys/aboutcanada/ui/main/MainViewModelTest.java
@@ -0,0 +1,34 @@
+package com.infosys.aboutcanada.ui.main;
+
+import com.infosys.aboutcanada.R;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+
+import static org.junit.Assert.assertNotNull;
+
+@RunWith(RobolectricTestRunner.class)
+public class MainViewModelTest {
+    private MainActivity activity;
+
+    @Before
+    public void setUp() throws Exception {
+        activity = Robolectric.buildActivity(MainActivity.class)
+                .create()
+                .resume()
+                .get();
+    }
+
+    @Test
+    public void shouldNotBeNull() throws Exception {
+        assertNotNull(activity);
+    }
+
+    @Test
+    public void shouldHaveWelcomeFragment() throws Exception {
+        assertNotNull(activity.getFragmentManager().findFragmentById(R.id.container));
+    }
+}
